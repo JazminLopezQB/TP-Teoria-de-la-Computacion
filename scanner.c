1f7b2d58f77b49cf7b2ee0d57c5e4d28f1a74c4d
lopez <jrlopez@fie.undef.edu.ar>
2025-11-15 19:33:30 -0300

    Codigo optimizado

--- scanner.c
+++ scanner.c
@@ -1,4 +1,10 @@
 #include "scanner.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/* ---------------------------------------------------------
+    FUNCIONES AUXILIARES
+   --------------------------------------------------------- */
 
 FILE *abrir_archivo(char nombre[]) {
     FILE *archivo = fopen(nombre, "r");
@@ -19,64 +25,127 @@ int es_constante(char cadena[]){
     return 1;
     }
 
+int es_separador_simple(int c) {
+    return c==';' || c==',' || c=='*' || c=='/' || c=='+' || c=='-' || c=='(' || c==')';
+    }
+
+int es_separador_especial(int c){
+    return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\0';
+    }
+
+int es_caracter_valido(int c) {
+    if (es_separador_simple(c) || es_separador_especial(c) || isalnum(c) || c==':' || c=='=') return 1;
+    return 0;
+    }
+
+void mostrar_identificador(char token[], int *ti, int tipo) {
+    if (token[0] == '\0') return;
+    if (tipo == 1)
+        printf("Constante '%s'\n", token);
+    else {
+        if(!isalpha(token[0]))
+            printf("Error, identificador invalido '%s'\n", token);
+        else
+            printf("Identificador '%s'\n", token);
+        }
+    token[0] = '\0';
+    *ti = 0;
+    }
+
+/* ---------------------------------------------------------
+    SCANNER PRINCIPAL
+   --------------------------------------------------------- */
 void scanner() {
 
     FILE *entrada = abrir_archivo("../entrada.txt");
 
-    // Definir variables de análisis
-    
-    char token[LONGITUD]; // Se van guardando lo que se lee del archivo
-    char separadores[] = " ;*/+-,=:()\n\t"; // Cosas que pueden provocar que se corte un identificador
-    char errores[] = "@!^$"; // Caracteres que producen errores
-    char caracter; // Entrada del archivo
-    int i = 0; // Indice para escribir caracter en token
+    char token[LONGITUD];
+    token[0] = '\0';
+    int ti = 0;
+    int modo_activo = 0;
 
-    while ((caracter = fgetc(entrada)) != EOF) { //TODO: Revisar por que strcmp(token, "fin\0") != 0 no anda
-        // TODO: También habria que poner un condicional para que descarte cualquier cosa que venga antes de "inicio"
-        if (strchr(separadores, caracter) != NULL || i >= LONGITUD - 1) { // Si encuentra un separador o no hay espacio
-            if (i > 0){
-                token[i] = '\0';
-                if (es_constante(token))
-                    printf("Constante: '%s'\n", token);
-                else
-                    printf("Identificador: '%s'\n", token);
-                i = 0; // Reinicio el indice
-            }
-            }
-        else {
-            token[i] = caracter;
-            i++;
-            }
-        switch (caracter) {
-            case ';':
-                printf("Punto y coma: '%c'\n", caracter);
-                break;
-            case ',':
-                printf("Coma: '%c'\n", caracter);
-                break;
-            case '*':
-                printf("Multiplicacion: '%c'\n", caracter);
-                break;
-            case '/':
-                printf("Division: '%c'\n", caracter);
-                break;
-            case '+':
-                printf("Suma: '%c'\n", caracter);
-                break;
-            case '-':
-                printf("Resta: '%c'\n", caracter);
-                break;
-            case '(':
-                printf("Parentesis que abre: '%c'\n", caracter);
-                break;
-            case ')':
-                printf("Parentesis que cierra: '%c'\n", caracter);
-                break;
-            default:
+    int c;
+    while ((c = fgetc(entrada)) != EOF) {
+
+        if (!modo_activo){
+
+            if (!isalpha(c)){continue;}
+            
+            token[0] = c;
+            ti++;
+            while ((c = fgetc(entrada)) != EOF && !es_separador_especial(c)) {
+                if (ti < LONGITUD - 1) token[ti++] = c;
+                }
+                token[ti] = '\0';
+            
+            if (strcmp(token, "inicio") == 0) {
+                modo_activo = 1;
+                mostrar_identificador(token, &ti, 0);
                 continue;
-                break;
+                }
+            ti = 0;
+            if (c != EOF) ungetc(c, entrada);
+            }
+        else{
+            if (isalnum(c)) {
+                token[0] = c;
+                ti = 1;
+                while ((c = fgetc(entrada)) != EOF && isalnum(c)) {
+                    if (ti < LONGITUD - 1) token[ti++] = c;
+                    }
+                token[ti] = '\0';
+
+                if (strcmp(token, "fin") == 0) {
+                    mostrar_identificador(token, &ti, 0);
+                    fclose(entrada);
+                    return;
+                    }
+                mostrar_identificador(token, &ti, es_constante(token));
+
+                if (c != EOF) ungetc(c, entrada);
+                continue;
+                }
+            else if (c == '=') {
+                printf("Error en asignación solo vino '='\n");
+                continue;
+                }
+            else if (c == ':') {
+                int c = fgetc(entrada);
+                if (c == '=') { printf("Asignación ':='\n"); }
+                else { printf("Error en asignación solo vino ':'\n"); }
+                continue;
+                }
+            else if (es_separador_simple(c)) {
+                switch (c) {
+                    case ';': printf("Punto y coma ';'\n"); break;
+                    case ',': printf("Coma ','\n"); break;
+                    case '*': printf("Multiplicación '*'\n"); break;
+                    case '/': printf("División '/'\n"); break;
+                    case '+': printf("Más '+'\n"); break;
+                    case '-': printf("Menos '-'\n"); break;
+                    case '(': printf("Paréntesis que abre '('\n"); break;
+                    case ')': printf("Paréntesis que cierra ')'\n"); break;
+                    }
+                continue; 
+                }
+            else if (!es_caracter_valido(c)){
+                char errbuf[LONGITUD*2];
+                int ei = 0;
+                errbuf[ei++] = (char)c;
+
+                while ((c = fgetc(entrada)) != EOF && !es_caracter_valido(c)) {
+                    if (ei < (int)sizeof(errbuf)-1) errbuf[ei++] = (char)c;
+                }
+                errbuf[ei] = '\0';
+
+                printf("Error general '%s'\n", errbuf);
+
+                if (c != EOF) ungetc(c, entrada);
+                continue;
+                }
             }
         }
 
+    printf("Error, no hay un identificador 'fin'.\n");
     fclose(entrada);
     }
--- scanner.h
+++ scanner.h
@@ -13,7 +13,11 @@
 #define LONGITUD 33 // Los identificadores tienen como longitud máxima 32 bits
 
 FILE *abrir_archivo(char nombre[]);
-int es_constante(char *cadena);
+int es_constante(char cadena[]);
+int es_separador_simple(int c);
+int es_separador_especial(int c);
+int es_caracter_valido(int c);
+void mostrar_identificador(char token[], int *ti, int tipo);
 void scanner();
 
 #endif // TP_FINAL_SCANNER_H
