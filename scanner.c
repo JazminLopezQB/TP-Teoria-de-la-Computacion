3e9fa70e303a7d00afdd3250d9629b397c962fb0
JazminLopezQB <162722391+JazminLopezQB@users.noreply.github.com>
2025-11-17 08:40:37 -0300

    Correcciones del profesor

--- scanner.c
+++ scanner.c
@@ -1,6 +1,4 @@
 #include "scanner.h"
-#include <ctype.h>
-#include <stdio.h>
 
 /* ---------------------------------------------------------
     FUNCIONES AUXILIARES
@@ -38,117 +36,91 @@ int es_caracter_valido(int c) {
     return 0;
     }
 
-void mostrar_identificador(char token[], int *ti, int tipo) {
+void mostrar_identificador(char token[], int tipo) {
     if (token[0] == '\0') return;
-    if (tipo == 1)
+    if (tipo)
         printf("Constante '%s'\n", token);
     else {
-        if(!isalpha(token[0]))
-            printf("Error, identificador invalido '%s'\n", token);
+        if (strlen(token) == 1){
+            switch (token[0]) {
+                case ';': printf("Punto y coma ';'\n"); return;
+                case ',': printf("Coma ','\n"); return;
+                case '*': printf("Multiplicación '*'\n"); return;
+                case '/': printf("División '/'\n"); return;
+                case '+': printf("Más '+'\n"); return;
+                case '-': printf("Menos '-'\n"); return;
+                case '(': printf("Paréntesis que abre '('\n"); return;
+                case ')': printf("Paréntesis que cierra ')'\n"); return;
+                case '=':
+                case ':': printf("Error en asignación solo vino '%s'\n", token); return;
+                default: break;
+                }
+            }
+        else if (strcmp(token, ":=") == 0){
+            printf("Asignación '%s'\n", token);
+            return;
+            }
+        else if(!isalpha(token[0]) && !es_caracter_valido(token[0])){
+            printf("Error general '%s'\n", token);
+            return;
+            }
+        else if (strcmp(token, "EOF") == 0){
+            printf("Error, no hay un programa valido.\n");
+            return;
+            }
         else
             printf("Identificador '%s'\n", token);
         }
-    token[0] = '\0';
-    *ti = 0;
     }
 
 /* ---------------------------------------------------------
     SCANNER PRINCIPAL
    --------------------------------------------------------- */
-void scanner() {
+char * scanner(FILE *entrada) {
 
-    FILE *entrada = abrir_archivo("../entrada.txt");
-
-    char token[LONGITUD];
+    static char token[LONGITUD];
     token[0] = '\0';
     int ti = 0;
-    int modo_activo = 0;
-
     int c;
+
     while ((c = fgetc(entrada)) != EOF) {
-
-        if (!modo_activo){
-
-            if (!isalpha(c)){continue;}
-            
-            token[0] = c;
-            ti++;
-            while ((c = fgetc(entrada)) != EOF && !es_caracter_valido(c)) {
-                if (ti < LONGITUD - 1) token[ti++] = c;
-                }
-                token[ti] = '\0';
-            
-            if (strcmp(token, "inicio") == 0) {
-                modo_activo = 1;
-                mostrar_identificador(token, &ti, 0);
-                continue;
-                }
-            ti = 0;
-            if (c != EOF) ungetc(c, entrada);
-            }
-        else if (!es_separador_especial(c)){
-            if (isalnum(c)) {
-                token[0] = c;
-                ti = 1;
-                while ((c = fgetc(entrada)) != EOF && isalnum(c)) {
-                    if (ti < LONGITUD - 1) token[ti++] = c;
+        if (!es_separador_especial(c)){
+            if (isalnum(c) || !es_caracter_valido(c)) {
+                token[ti++] = c;
+                if(isalnum(c)){
+                    while ((c = fgetc(entrada)) != EOF && isalnum(c)) {
+                        if (ti < LONGITUD - 1) token[ti++] = c;
+                        }
+                    }
+                else if (!es_caracter_valido(c)){
+                    while ((c = fgetc(entrada)) != EOF && !es_caracter_valido(c) && !isalnum(c)) {
+                        if (ti < (int)sizeof(token)-1) token[ti++] = c;
+                        }
                     }
                 token[ti] = '\0';
-
-                if (strcmp(token, "fin") == 0) {
-                    mostrar_identificador(token, &ti, 0);
-                    fclose(entrada);
-                    return;
-                    }
-                mostrar_identificador(token, &ti, es_constante(token));
-
                 if (c != EOF) ungetc(c, entrada);
-                continue;
+                return token;
                 }
-            else if (c == '=') {
-                printf("Error en asignación solo vino '='\n");
-                continue;
+            else if (es_separador_simple(c) || c == '=') {
+                token[0] = c;
+                token[1] = '\0';
+                return  token;
                 }
             else if (c == ':') {
                 int c = fgetc(entrada);
-                if (c == '=') { printf("Asignación ':='\n"); }
+                if (c == '=') {
+                    token[0] = ':';
+                    token[1] = '=';
+                    token[2] = '\0';
+                    }
                 else {
-                    printf("Error en asignación solo vino ':'\n");
+                    token[0] = ':';
+                    token[1] = '\0';
                     if (c != EOF) ungetc(c, entrada);
                     }
-                continue;
-                }
-            else if (es_separador_simple(c)) {
-                switch (c) {
-                    case ';': printf("Punto y coma ';'\n"); break;
-                    case ',': printf("Coma ','\n"); break;
-                    case '*': printf("Multiplicación '*'\n"); break;
-                    case '/': printf("División '/'\n"); break;
-                    case '+': printf("Más '+'\n"); break;
-                    case '-': printf("Menos '-'\n"); break;
-                    case '(': printf("Paréntesis que abre '('\n"); break;
-                    case ')': printf("Paréntesis que cierra ')'\n"); break;
-                    }
-                continue; 
-                }
-            else if (!es_caracter_valido(c) && !isalnum(c)){
-                char errbuf[LONGITUD*2];
-                int ei = 0;
-                errbuf[ei++] = (char)c;
-
-                while ((c = fgetc(entrada)) != EOF && !es_caracter_valido(c) && !isalnum(c)) {
-                    if (ei < (int)sizeof(errbuf)-1) errbuf[ei++] = (char)c;
-                    }
-                errbuf[ei] = '\0';
-
-                printf("Error general '%s'\n", errbuf);
-
-                if (c != EOF) ungetc(c, entrada);
-                continue;
+                return token;
                 }
             }
         }
-
-    printf("Error, no hay un identificador 'fin'.\n");
-    fclose(entrada);
+    return "EOF";
     }
