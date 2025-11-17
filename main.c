2d2651f43cf824699112c74e805a912b58187c6c
JazminLopezQB <162722391+JazminLopezQB@users.noreply.github.com>
2025-11-17 08:40:00 -0300

    Correcciones del profesor

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include "scanner.h"
 
 int main() {
-    scanner();
+    FILE *entrada = abrir_archivo("../entrada.txt");
+    int modo_activo = 0;
+    char *token = NULL;
+
+    do {
+        token = scanner(entrada);
+        if (strcmp(token, "inicio") == 0) {modo_activo = 1;}
+        if (modo_activo) {mostrar_identificador(token, es_constante(token));}
+        }while (strcmp(token, "fin") != 0);
+
+    fclose(entrada);
     return 0;
     }
